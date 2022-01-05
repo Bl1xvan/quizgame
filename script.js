7ba56ad24fd3cc660c7a7bbc8c9f035fb75bc2c6
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-01-05 09:37:49 -0500

    just about to add function to object array

---
+++ script.js
@@ -0,0 +1,40 @@
+const quizQuestions = [
+    {
+        Qnumber: 1,
+        Qquestion: "food",
+        Qanswer1: "Bananas",
+        Qanswer2: "Chocolate",
+        Qanswer3: "Chicken",
+        Qanswer4: "Pizza"
+    },
+
+    {
+        Qnumber: 2, 
+        Qquestion: "color?",
+        Qanswer1: "red",
+        Qanswer2: "green",
+        Qanswer3: "silver",
+        Qanswer4: "purple"
+    },
+
+    {
+        Qnumber: 3, 
+        Qquestion: "animal",
+        Qanswer1: "cat",
+        Qanswer2: "dog",
+        Qanswer3: "rabbit",
+        Qanswer4: "snake"
+    },
+
+    {
+        Qnumber: 4,
+        Qquestion: "number",
+        Qanswer1: "1",
+        Qanswer2: "2",
+        Qanswer3: "3",
+        Qanswer4: "4"
+    },
+
+    
+]
+
